a4fa8b5bb41728d2a4ca0b04e2fa402f73a44b63
Urwashi <urwashistudy@gmail.com>
2025-11-05 10:15:01 +0530

    formatting

--- routes/product.js
+++ routes/product.js
@@ -4,27 +4,27 @@ var router = express.Router();
 const Product = require('../models/product')
 
 router.get('/', async (req, res, next) => {
-  // res.json(users)
-  const product = await Product.find();
-  res.json(product)
+    // res.json(users)
+    const product = await Product.find();
+    res.json(product)
 });
 
 router.get('/:id', async (req, res, next) => {
-  console.log(req.params.id)
-  const productId = req.params.id
-  const product = await Product.findById(productId)
-  if (!product) return res.status(404).json({ message: 'Product not found' })
-  res.json(product)
+    console.log(req.params.id)
+    const productId = req.params.id
+    const product = await Product.findById(productId)
+    if (!product) return res.status(404).json({ message: 'Product not found' })
+    res.json(product)
 });
 
 router.post('/', async (req, res, next) => {
-  const { name, description,price,category,images } = req.body;
-  if (!name || !description||!price||!category) {
-    return res.status(400).json({ message: 'Invalid request' })
-  }
-  const newProduct= new Product(req.body)
-  await newProduct.save()
-  res.status(201).json(newProduct)
+    const { name, description, price, category, images } = req.body;
+    if (!name || !description || !price || !category) {
+        return res.status(400).json({ message: 'Invalid request' })
+    }
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.status(201).json(newProduct)
 })
 
-module.exports=router
+module.exports = router
