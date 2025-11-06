93bf521ec0d25105f360c231871e929b38413c7f
Urwashi <urwashistudy@gmail.com>
2025-11-06 10:14:33 +0530

    Create order model and orders api

---
+++ models/order.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            }
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'shipped', 'outForDelivery', 'delivery', 'cancelled'],
+        default: 'pending'
+    },
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['pending', 'paid', 'failed'],
+        default: 'pending'
+    },
+    shippingAddress: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order
--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,87 @@
 const express = require('express')
 const router = express.Router()
+const Order = require('../models/order')
+const Product = require('../models/product')
 
-router.get('/', function (req, res, next) {
-    res.send('Pointing to orders router')
+router.get('/', async (req, res) => {
+    try {
+        const orders = await Order.find().populate('userId', 'name email')
+        res.json(orders)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Error Fetching orders' })
+    }
+});
+
+router.get('/:ordeId', async (req, res) => {
+    console.log(req.params.ordeId)
+    const orderId = req.params.ordeId
+    const order = await Order.findById(orderId).populate('userId', 'name email')
+    if (!order) return res.status(404).json({ message: 'Order not found' })
+    res.json(order)
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { products, userId, paymentMethod, shippingAddress } = req.body;
+
+        //validate product
+        const productIds = products.map((product => product.productId))//Get all product Id related to orders
+        const dbProducts = await Product.find({ _id: { $in: productIds } })
+
+        if (dbProducts.length !== products.length) {
+            return res.status(400).json({ message: 'Invaild product ID' })
+        }
+
+        let totalPrice = 0;
+        products.forEach(product => {
+            const dbProduct = dbProducts.find(p => p._id.toString() === product.productId)
+            totalPrice += dbProduct.price * product.quantity
+        });
+
+        const newOrder = new Order({
+            userId,
+            products,
+            totalPrice,
+            paymentMethod,
+            shippingAddress
+        })
+        await newOrder.save()
+        res.status(201).json(newOrder)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error occured while creating order' })
+    }
 })
+
+// router.put('/:id', async (req, res, next) => {
+//     try {
+//         const userId = req.params.id
+//         const updates = req.body
+//         const updatedUser = await User.findByIdAndUpdate(userId, updates, {
+//             new: true
+//         })
+//         if (!updatedUser) {
+//             return res.status(404).send({ message: 'User not found' })
+//         }
+//         res.send(updatedUser)
+//     } catch (err) {
+//         console.error(err)
+//         res.status(400).send({ message: 'Error updating this user' })
+//     }
+
+// })
+
+// router.delete('/:id', async (req, res, next) => {
+//     try {
+//         const userId = req.params.id;
+//         await User.findByIdAndDelete(userId)
+//         res.status(204).send()
+//     }
+//     catch (err) {
+//         console.error(err)
+//         res.status(500).send({ message: 'Error while deleting the user' })
+//     }
+// })
 
 module.exports = router
